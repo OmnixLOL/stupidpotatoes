f816c7bdec97fbd52670909f522605a208932510
TomuktadirXD <muktamahmuda400@gmail.com>
2026-02-25 16:44:36 +0600

    Fixed loading issue

--- src/main/java/org/omnix/stupot/item/ModItems.java
+++ src/main/java/org/omnix/stupot/item/ModItems.java
@@ -5,23 +5,24 @@ import org.omnix.stupot.Stupidpotatoes;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryKey;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-    public static final Item STUPID_POTATO = register("stupid_potato",
-            new Item(new Item.Settings()));
 
-    private static Item register(String name, Item item) {
-        return Registry.register(
-                Registries.ITEM,
-                Identifier.of(Stupidpotatoes.MOD_ID, name),
-                item
-        );
+    public static final Item STUPID_POTATO = register("stupid_potato");
+
+    private static Item register(String name) {
+        Identifier id = Identifier.of(Stupidpotatoes.MOD_ID, name);
+
+        RegistryKey<Item> key = RegistryKey.of(Registries.ITEM.getKey(), id);
+
+        Item item = new Item(new Item.Settings().registryKey(key));
+
+        return Registry.register(Registries.ITEM, id, item);
     }
 
     public static void registerModItems() {
         Stupidpotatoes.LOGGER.info("Registering Mod Items for " + Stupidpotatoes.MOD_ID);
-
-        Item unused = STUPID_POTATO;
     }
 }
